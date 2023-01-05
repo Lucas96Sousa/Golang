56fb2f16fb89efc7a5b0f84ba42e781993806097
Lucas Oliveira <lucas996oliveira@gmail.com>
2023-01-05 15:36:19 -0300

    Create Method: Delete User

--- golang_course/17-CRUD/main.go
+++ golang_course/17-CRUD/main.go
@@ -16,6 +16,7 @@ func main() {
   router.HandleFunc("/users", server.FindUser).Methods(http.MethodGet)
   router.HandleFunc("/users/{id}", server.FindUserById).Methods(http.MethodGet)
   router.HandleFunc("/users/{id}", server.UpdateUser).Methods(http.MethodPut)
+  router.HandleFunc("/users/{id}", server.DeleteUser).Methods(http.MethodDelete)
 
 	fmt.Println("http://localhost:5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
