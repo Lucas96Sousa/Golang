2d87d60bf7cfdfa542e117af90950e8edba66435
Lucas Oliveira <lucas996oliveira@gmail.com>
2023-01-08 02:34:00 -0300

    Create a login route

--- API-DEVBOOK/src/router/routes/login.go
+++ API-DEVBOOK/src/router/routes/login.go
@@ -1,1 +1,10 @@
-package login
+package routes
+
+import "net/http"
+
+var routeLogin = Route{
+	URI:         "/login",
+	Method:      http.MethodPost,
+	Func:        func(w http.ResponseWriter, r *http.Request) {},
+	RequestAuth: false,
+}
